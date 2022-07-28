fab8f300fe70df1814d13f76a488b27fec4619c8
javyer <70788090+javyer12@users.noreply.github.com>
2022-07-27 21:08:31 -0600

    update header

--- src/assets/main.js
+++ src/assets/main.js
@@ -59,6 +59,6 @@ async function fetchData(urlApi) {
 
 //install github-pages
 //npm install gh-pages --save-dev
-{
+
   /* <iframe width="560" height="315" src="https://www.youtube.com/embed/GSIkghMhFF4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> */
-}
+
